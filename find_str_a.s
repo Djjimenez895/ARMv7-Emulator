0f738756e0884b7fe2d2c69946f3899d9788d638
Davin Jimenez <31362013+Djjimenez895@users.noreply.github.com>
2019-09-16 12:11:54 -0700

    Edited comments, deleted a couple comments

--- find_str_a.s
+++ find_str_a.s
@@ -30,44 +30,43 @@ find_str_a:
     mov r4, #0 /* i */
     mov r5, #0 /* j */
 
-/*loop for finding the string length */
+/* loop for finding the string length */
 str_length_loop:
     /* r4 will be used to find length */
     /* r2 will load in each byte */
     ldrb r2, [r0]
     cmp r2, #0
-    beq find_length_sub /*if equal, it's the end of the string */
+    beq find_length_sub /* if equal, it's the end of the string */
     add r4, r4, #1 /* increments length by 1 */
     add r0, r0, #1 /* moves over a byte in the string */
     b str_length_loop
 
-/*loop for finding substring length */
+/* loop for finding substring length */
 sub_length_loop:
     ldrb r2, [r0]
     cmp r2, #0
     beq find_str_continue
-    add r4, r4, #1 /*increments length by 1 */
-    add r0, r0, #1 /*moves over a byte in the string */
+    add r4, r4, #1 /* increments length by 1 */
+    add r0, r0, #1 /* moves over a byte in the string */
     b sub_length_loop
 
 /* Label used for cleaning up some registers before finding substring length */
 find_length_sub:
-    mov r7, r4 /*puts the length in r7 (s_len) */
-    mov r4, #0 /*reset counter*/
-    mov r0, r1 /*moves substring into r0 */
+    mov r7, r4 /* puts the length in r7 (s_len) */
+    mov r4, #0 /* reset counter */
+    mov r0, r1 /* moves substring into r0 */
     b sub_length_loop
 
 find_str_continue:
-    /* mov r0, r7 and then returning seems to work correctly*/
-    mov r6, r4 /*moving length of substring into r6 (sub_len) */
-    ldr r0, [sp] /*load in the original r0 into r0*/
-    ldr r1, [sp, #4] /* load in the original r1 into r1*/
+    /* mov r0, r7 and then returning seems to work correctly */
+    mov r6, r4 /* moving length of substring into r6 (sub_len) */
+    ldr r0, [sp] /* load in the original r0 into r0 */
+    ldr r1, [sp, #4] /* load in the original r1 into r1 */
     ldrb r9, [r0] /* loads in first byte of r0 */
-    ldrb r10, [r1] /* loads in first byte of r1*/
+    ldrb r10, [r1] /* loads in first byte of r1 */
     mov r4, #0 /* resetting i */
     mov r5, #0 /* resetting j */
-    mvn r8, #0 /* sub_first_index*/
-    //b find_str_loop
+    mvn r8, #0 /* sub_first_index */
     
 find_str_loop:
 
@@ -77,42 +76,42 @@ find_str_loop:
     cmp r5, r6 /* checks if j < sub_len */
     bge done
     
-    /*Keep an eye on these four lines */
+    /* Keep an eye on these four lines */
     cmp r9, r10 /* s[i] == sub[j] comparison */
     beq equal_chars /* branches if the values are equal; the if condition */
-    cmpne r5, #0 /*else if j != 0 */
+    cmpne r5, #0 /* else if j != 0 */
     bne unequal_chars
 
-    /*if neither condition is met */
-    add r4, r4, #1 /*i++ */
+    /* if neither condition is met */
+    add r4, r4, #1 /* i++ */
     add r0, r0, #1 /* shift over one byte in r0 */
     ldrb r9, [r0] /* loads in the next byte in r0 */
     b find_str_loop
 
-/*Since I know this is the condition that is met, the loop continues */
-/*when it branches back to find_str_loop */    
+/* Since I know this is the condition that is met, the loop continues */
+/* when it branches back to find_str_loop */    
 equal_chars:
-    cmp r5, #0 /*if j == 0 condition */
+    cmp r5, #0 /* if j == 0 condition */
     moveq r8, r4 /* sub_first_index = i  */
     add r5, r5, #1 /* j++ */
-    add r1, r1, #1 /* shift over one byte in r1*/
+    add r1, r1, #1 /* shift over one byte in r1 */
     ldrb r10, [r1] /* loading in next element in substring */
-    /*because j is being incremented by 1 */
+    /* because j is being incremented by 1 */
 
     /* increments i before going to the start of the loop again */
     add r4, r4, #1 /* i++ */
-    add r0, r0, #1 /* shift over one byte in r0*/
+    add r0, r0, #1 /* shift over one byte in r0 */
     ldrb r9, [r0] /* loads in the next byte in r0 */
     b find_str_loop
 
 unequal_chars:
     ldr r1, [sp, #4] /* resets to the beginning of the substring */
-    /*so that r1 points at the first address in the substring */
+    /* so that r1 points at the first address in the substring */
     ldrb r10, [r1]
-    mov r5, #0 /*j = 0, resets j */
+    mov r5, #0 /* j = 0, resets j */
     mvn r8, #0 /* sub_first_index = -1 */
-    cmp r9, r10 /*if s[i] == sub[j] */
-    moveq r8, r4 /*sub_first_index = i */
+    cmp r9, r10 /* if s[i] == sub[j] */
+    moveq r8, r4 /* sub_first_index = i */
     addeq r5, r5, #1 /* j++ */
     addeq r1, r1, #1 /* move over one byte*/
     ldrb r10, [r1] /* load in the byte */
@@ -124,9 +123,9 @@ unequal_chars:
     b find_str_loop
             
 done:
-    cmp r5, r6 /*this comparison is used to check the case where */
-    /*the string has ended and the substring wasn't found, but */
-    /*r8 (sub_first_index) has not been reset*/
+    cmp r5, r6 /* this comparison is used to check the case where */
+    /* the string has ended and the substring wasn't found, but */
+    /* r8 (sub_first_index) has not been reset */
     mvnne r8, #0
     mov r0, r8 /* r0 = sub_first_index so that it will be returned */
     
